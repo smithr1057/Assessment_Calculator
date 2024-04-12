1d43730a5327a862788a120dfa2a5d3ee521e521
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2024-04-12 13:26:51 +1200

    Integrated 03_pick_shape_v2

--- 01_Calc_Base.py
+++ 01_Calc_Base.py
@@ -1,12 +1,36 @@
-import colorama
+import math
 # Functions
+
+
+# Lets you change color of printed text easily
+def color_text(text, color):
+    # Code was found using chatGpt using prompt
+    # "Python function that allows me to change the text color"
+    # Code was changed a bit as some parts were unneeded
+
+    # list of colors
+    colors = {
+        'red': '\033[91m',
+        'green': '\033[92m',
+        'yellow': '\033[93m',
+        'blue': '\033[94m',
+        'magenta': '\033[95m',
+        'cyan': '\033[96m',
+        'white': '\033[97m',
+    }
+
+    # Prints text in specified color
+    return f"{colors[color]}{text}\033[0m"
 
 
 # checks user answers with valid answer
 def string_checker(question, num_letters, valid_list, custom_error=None):
 
     if custom_error is None:
-        error = f"Please choose {valid_list[0]} or {valid_list[1]}"
+        if len(valid_list) == 3:
+            error = f"Please choose {valid_list[0]}, {valid_list[1]} or {valid_list[2]}"
+        else:
+            error = f"Please choose {valid_list[0]} or {valid_list[1]}"
     else:
         error = custom_error
 
@@ -28,8 +52,51 @@ def string_checker(question, num_letters, valid_list, custom_error=None):
                     return i
 
         # output error if item not in list
-        print(colorama.Fore.RED + error)
+        print(color_text(error, 'red'))
         print()
+
+
+# checks users enter a float between a low and high number
+def num_check(question, low=None, high=None):
+    # Used ChatGPT to allow the use of the letter 'x' used the prompt bellow
+    # Make that function allow the letter 'x' to be used
+
+    situation = ''
+    if low is not None and high is not None:
+        situation = "both"
+    elif low is not None and high is None:
+        situation = "low only"
+
+    while True:
+        try:
+            # Ask the question
+            response = input(question)
+
+            # Check if response is 'xxx'
+            if response.lower() == 'xxx':
+                return response
+
+            # Convert the response to a float
+            response = float(response)
+
+            # Checks input is not too high or
+            # too low if both upper and lower bounds are specified
+            if situation == "both":
+                if response < low or response > high:
+                    color_text(f"Please enter a number between {low} and {high}", 'red')
+                    continue
+
+            # Checks input is not too low
+            elif situation == "low only":
+                if response <= low:
+                    color_text(f"Please enter a number that is more than {low}", 'red')
+                    continue
+
+            return response
+
+        except ValueError:
+            color_text("Please enter a number", 'red')
+            continue
 
 
 # Displays instructions
@@ -40,11 +107,12 @@ def instructions():
 # Prints out the valid shapes from the necessary list
 def print_valid_shapes(dim, shape_list):
     print(f"*** Valid {dim} Shapes ***")
-    print(", ".join(shape_list))
+    print(", ".join(shape_list[:-1]))  # Exclude 'shapes'
 
 
 # Asks user for the shape they want and returns it
 def get_user_input(var_dimension):
+
     # set up valid shape lists including the word 'shapes'
     shapes_2d = ['circle', 'square', 'rectangle', 'triangle', 'shapes']
     shapes_3d = ['cuboid', 'cylinder', 'triangular prism', 'cone',
@@ -67,9 +135,18 @@ def get_user_input(var_dimension):
 
 
 # Main Routine
+
+# Set pi value
+pi = math.pi
+
 # Lists
 yn_list = ['yes', 'no']
 dimension_list = ['2d', '3d']
+area_perm_list = ['area', 'perimeter', 'both']
+vol_face_list = ['volume', 'surface area', 'both']
+
+
+# Dictionaries
 
 # Asks user if they want to read instructions, if yes output instructions
 show_instructions = string_checker("Do you want to read the instructions? ", 1, yn_list)
@@ -79,4 +156,18 @@ if show_instructions == 'yes':
 
 # Loop code until user quits
 while True:
-    pass
+    # Ask user to choose between 2d or 3d shapes
+    dimension = string_checker('2D or 3D shape? ', 1, ['2d', '3d'])
+
+    # Asks user for a valid shape as per earlier choice
+    user_choice = get_user_input(dimension)
+
+    print()
+
+    if dimension == '2d':
+        area_perimeter = string_checker("Do you want area, perimeter or both calculated? ", 1, area_perm_list)
+    else:
+        vol_face = string_checker('Do you want the volume, surface area or both calculated? ', 1, vol_face_list)
+
+
+
