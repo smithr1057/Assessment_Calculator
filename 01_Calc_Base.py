2a8a8e9947aa6e30fc786b2f591eaa4c0e3d9b9a
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2024-04-30 12:30:09 +1200

    Integrated calc_shapes_func

--- 01_Calc_Base.py
+++ 01_Calc_Base.py
@@ -106,8 +106,10 @@ def instructions():
 
 # Prints out the valid shapes from the necessary list
 def print_valid_shapes(dim, shape_list):
+    print()
     print(f"*** Valid {dim} Shapes ***")
     print(", ".join(shape_list[:-1]))  # Exclude 'shapes'
+    print()
 
 
 # Asks user for the shape they want and returns it
@@ -134,6 +136,155 @@ def get_user_input(var_dimension):
             return shape
 
 
+# Calculates shapes area and perimeter /
+# volume and surface area and prints out the answer
+def calc_shape(shape, dimension, to_calculate):
+
+    # Set answer values
+    answer_one = answer_two = ''
+
+    # Set input prompts and shape calculations
+    if dimension == '2d':
+
+        to_calculate_list = ['area', 'perimeter', 'both']
+
+        # Dictionary containing prompts and units for each input
+        input_prompts = {
+            'circle': {'radius': ('radius', 'Enter radius of the circle (half the diameter): ', '')},
+            'square': {'length': ('length', 'Enter length of the square: ', '')},
+            'rectangle': {'length': ('length', 'Enter length of the rectangle: ', ''),
+                          'width': ('width', 'Enter width of the rectangle: ', '')},
+            'triangle': {'side1': ('side1', 'Enter the length of one of the sides: ', ''),
+                         'side2': ('side2', 'Enter the length of a different side: ', ''),
+                         'side3': ('side3', 'Enter the length of the last side: ', '')}
+        }
+
+        # Dictionary containing formulas for area and perimeter calculation for each shape
+        shape_calculations = {
+            'circle': {
+                'area': lambda r: pi * r ** 2,
+                'perimeter': lambda r: 2 * pi * r
+            },
+            'square': {
+                'area': lambda s: s ** 2,
+                'perimeter': lambda s: 4 * s
+            },
+            'rectangle': {
+                'area': lambda l, w: l * w,
+                'perimeter': lambda l, w: 2 * (l + w)
+            },
+            'triangle': {
+                'area': lambda s1, s2, s3: (((s1 + s2 + s3) / 2) *
+                                            (((s1 + s2 + s3) / 2) - s1) *
+                                            (((s1 + s2 + s3) / 2) - s2) *
+                                            (((s1 + s2 + s3) / 2) - s3)) ** 0.5,
+                'perimeter': lambda s1, s2, s3: s1 + s2 + s3
+            }
+        }
+    else:
+
+        to_calculate_list = ['volume', 'surface area', 'both']
+
+        # Dictionary mapping shapes to their parameters and prompts
+        input_prompts = {
+            'cuboid': {
+                'length': ('length', 'Length: ', '')
+            },
+
+            'cylinder': {
+                'radius': ('radius', 'Radius: ', ''),
+                'height': ('height', 'Height: ', '')
+            },
+            'triangular prism': {
+                'side1': ('side1', 'Enter the length of one of the triangles sides: ', ''),
+                'side2': ('side2', 'Enter the length of a different side: ', ''),
+                'side3': ('side3', 'Enter the length of the last side: ', ''),
+                'length': ('length', 'Length: ', '')
+            },
+
+            'cone': {
+                'radius': ('radius', 'Radius: ', ''),
+                'height': ('height', 'Height: ', '')
+            },
+
+            'sphere': {
+                'radius': ('radius', 'Radius: ', '')
+            },
+
+            'square based pyramid': {
+                'width': ('width', 'Width: ', ''),
+                'height': ('height', 'Height: ', '')
+            },
+
+            'triangle based pyramid': {
+                'height': ('height', 'Height: ', ''),
+                'base_width': ('width', 'Width of the base triangle: ', ''),
+                'apothem': ('apothem', 'Apothem length: ', ''),
+                'sl_height': ('slant', 'Slant_height: ', '')
+            }
+        }
+
+        # Dictionary mapping shapes to their formulas for volume and surface area
+        shape_calculations = {
+            'cuboid': {
+                'volume': lambda l: l ** 3,
+                'surface area': lambda l: 2 * (l * l + l * l + l * l)
+            },
+            'cylinder': {
+                'volume': lambda r, h: pi * r ** 2 * h,
+                'surface area': lambda r, h: 2 * pi * r * h + 2 * pi * r ** 2
+            },
+            'triangular prism': {
+                'volume': lambda a, b, c, l: 1 / 4 * l * ((a + b + c) * (b + c - a) * (c + a - b) * (a + b - c)) ** 0.5,
+                'surface area': lambda a, b, c, l: l * (a + b + c) + 1 / 4 * (
+                            (a + b + c) * (b + c - a) * (c + a - b) * (a + b - c)) ** 0.5 + 1 / 4 * (
+                                                               (a + b + c) * (b + c - a) * (c + a - b) * (
+                                                                   a + b - c)) ** 0.5
+            },
+            'cone': {
+                'volume': lambda r, h: 1 / 3 * pi * r ** 2 * h,
+                'surface area': lambda r, h: pi * r * (r + (h ** 2 + r ** 2) ** 0.5)
+            },
+            'sphere': {
+                'volume': lambda r: 4 / 3 * pi * r ** 3,
+                'surface area': lambda r: 4 * pi * r ** 2
+            },
+            'square based pyramid': {
+                'volume': lambda w, h: w ** 2 * h / 3,
+                'surface area': lambda w, h: w ** 2 + 2 * w * (w ** 2 / 4 + h ** 2) ** 0.5
+            },
+            'triangle based pyramid': {
+                'volume': lambda bw, a, h: (1 / 6) * (0.5 * bw * a) * h,
+                'surface area': lambda bw, a, h, sl: (0.5 * bw * sl) + ((3 / 2) * sl)
+            }
+        }
+
+    # Retrieve prompts for user inputs associated with the chosen shape
+    prompts = input_prompts[shape]
+
+    # Create dictionary for user inputs needed for area calculation
+    inputs = {
+        # Get user input for each parameter then iterate over parameter and prompt
+        key: num_check(prompt[1], 0)
+        for key, prompt in prompts.items()
+    }
+
+    if to_calculate == to_calculate_list[0] or to_calculate_list[2]:
+        # Calculate area based on the chosen shape and user inputs
+        answer_one = shape_calculations[shape][to_calculate_list[0]](*inputs.values())
+
+    if to_calculate == to_calculate_list[1] or to_calculate_list[2]:
+        # Calculate perimeter based on the chosen shape and user inputs
+        answer_two = shape_calculations[shape][to_calculate_list[1]](*inputs.values())
+
+    print()
+    if to_calculate == to_calculate_list[0] or to_calculate_list[2]:
+        print(f'{to_calculate_list[0]}: {answer_one:.2f}')
+    if to_calculate == to_calculate_list[1] or to_calculate_list[2]:
+        print(f'{to_calculate_list[1]}: {answer_two:.2f}')
+    print()
+
+
 # Main Routine
 
 # Set pi value
@@ -157,17 +308,22 @@ if show_instructions == 'yes':
 # Loop code until user quits
 while True:
     # Ask user to choose between 2d or 3d shapes
-    dimension = string_checker('2D or 3D shape? ', 1, ['2d', '3d'])
+    dimensions = string_checker('2D or 3D shape? ', 1, ['2d', '3d', 'xxx'])
+
+    # If user enters 'xxx' quit
+    if dimensions == 'xxx':
+        break
 
     # Asks user for a valid shape as per earlier choice
-    user_choice = get_user_input(dimension)
+    user_shape = get_user_input(dimensions)
 
     print()
 
-    if dimension == '2d':
-        area_perimeter = string_checker("Do you want area, perimeter or both calculated? ", 1, area_perm_list)
+    if dimensions == '2d':
+        whats_calculated = string_checker("Do you want area, perimeter or both calculated? ", 1, area_perm_list)
     else:
-        vol_face = string_checker('Do you want the volume, surface area or both calculated? ', 1, vol_face_list)
+        whats_calculated = string_checker('Do you want the volume, surface area or both calculated? ', 1, vol_face_list)
 
+    calc_shape(user_shape, dimensions, whats_calculated)
 
-
+print(color_text('Thank you for using the Super Shape Calculator', 'green'))
